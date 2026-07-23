7e6774105ec97dcb1d11a2f85290ea66b495ca26
Nicholas Hastings <nshastings@gmail.com>
2026-07-22 20:29:26 -0400

    Add CI, x64 build, releases (#16)
    * Add CI, x64 build, releases
    
    * Fixes
    
    * More fixes

--- configure.py
+++ configure.py
@@ -2,22 +2,21 @@
 import sys
 from ambuild2 import run
 
-# Simple extensions do not need to modify this file.
-
-builder = run.PrepareBuild(sourcePath = sys.path[0])
-
-builder.options.add_option('--hl2sdk-root', type=str, dest='hl2sdk_root', default=None,
-		                   help='Root search folder for HL2SDKs')
-builder.options.add_option('--mms-path', type=str, dest='mms_path', default=None,
-                       help='Path to Metamod:Source')
-builder.options.add_option('--sm-path', type=str, dest='sm_path', default=None,
-                       help='Path to SourceMod')
-builder.options.add_option('--enable-debug', action='store_const', const='1', dest='debug',
-                       help='Enable debugging symbols')
-builder.options.add_option('--enable-optimize', action='store_const', const='1', dest='opt',
-                       help='Enable optimization')
-builder.options.add_option('-s', '--sdks', default='all', dest='sdks',
-                       help='Build against specified SDKs; valid args are "all", "present", or '
-                            'comma-delimited list of engine names (default: %default)')
-
-builder.Configure()
+parser = run.BuildParser(sourcePath=sys.path[0], api='2.2')
+parser.options.add_argument('--hl2sdk-root', type=str, dest='hl2sdk_root', default=None,
+                            help='Root search folder for HL2SDKs')
+parser.options.add_argument('--hl2sdk-manifest-path', type=str, dest='hl2sdk_manifest', default=None,
+                            help='Path to hl2sdk-manifests when it is not checked out locally')
+parser.options.add_argument('--mms-path', type=str, dest='mms_path', default=None,
+                            help='Path to Metamod:Source')
+parser.options.add_argument('--sm-path', type=str, dest='sm_path', default=None,
+                            help='Path to SourceMod')
+parser.options.add_argument('--enable-debug', action='store_const', const='1', dest='debug',
+                            help='Enable debugging symbols')
+parser.options.add_argument('--enable-optimize', action='store_const', const='1', dest='opt',
+                            help='Enable optimization')
+parser.options.add_argument('-s', '--sdks', default='present', dest='sdks',
+                            help='SDKs to build: all, present, or a comma-delimited manifest list')
+parser.options.add_argument('--targets', type=str, dest='targets', default=None,
+                            help='Target architectures, separated by commas (x86,x86_64)')
+parser.Configure()
